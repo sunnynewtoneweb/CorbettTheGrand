abf51fd018eb5bb83ef6e9f83ca32c4178027e03
sunnynewtoneweb <sunny.newtoneweb@gmail.com>
2025-11-03 20:11:55 +0530

    Form update
    form/buttons update

--- src/booka-room/booka-room.ts
+++ src/booka-room/booka-room.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+interface Room {
+  value: string;
+  viewValue: string;
+}
+interface Gst {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-booka-room',
-  imports: [],
   templateUrl: './booka-room.html',
-  styleUrl: './booka-room.css'
+  styleUrl: './booka-room.css',
+  imports: [MatFormFieldModule, MatDatepickerModule, FormsModule, MatSelectModule, MatInputModule, MatButtonModule, MatExpansionModule],
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookaRoom {
-
+rooms: Room[] = [
+    {value: '1-0', viewValue: '1'},
+    {value: '2-1', viewValue: '2'},
+    {value: '3-2', viewValue: '3'},
+  ];
+havegst: Gst[] = [
+    {value: 'Yes-0', viewValue: 'Yes'},
+    {value: 'No-1', viewValue: 'No'},
+  ];
 }
--- src/home/home.ts
+++ src/home/home.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
+import {MatButtonModule} from '@angular/material/button';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
+  imports: [MatFormFieldModule, MatDatepickerModule, MatInputModule, MatButtonModule, MatExpansionModule],
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Home {
 
